38aa72ff586dac639ee71dbb1ad45dfea70b7503
inventivetalent <mail@inventivetalent.org>
2019-04-15 18:57:07 +0200

    stitching

--- src/main/java/org/inventivetalent/regionpreview/Preview.java
+++ src/main/java/org/inventivetalent/regionpreview/Preview.java
@@ -33,6 +33,28 @@ public class Preview implements Callable<Boolean> {
 						description = "Output directory for generated images")
 	private File output = new File(".");
 
+	@CommandLine.Option(names = { "--stitch" },
+						description = "Whether to stitch the resulting images together into a large image, instead of outputting single files")
+	private boolean stitch = false;
+
+	@CommandLine.Option(names = { "--stitchWidth" },
+						description = "Number of regions to combine in the x-direction")
+	private int stitchWidth = 1;
+
+	@CommandLine.Option(names = { "--stitchHeight" },
+						description = "Number of regions to combine in the z-direction")
+	private int stitchHeight = 1;
+
+	@CommandLine.Option(names = { "--stitchStartX" },
+						description = "Where to start stitching in x-direction")
+	private int stitchStartX = 0;
+
+	@CommandLine.Option(names = { "--stitchStartZ" },
+						description = "Where to start stitching in z-direction")
+	private int stitchStartZ = 0;
+
+	private BufferedImage stitchedImage;
+
 	@Override
 	public Boolean call() throws Exception {
 		if (input == null || input.length == 0) {
@@ -40,7 +62,22 @@ public class Preview implements Callable<Boolean> {
 			return false;
 		}
 
-		System.out.println("Generating preview image in scale 1:" + scale + "...");
+		if (scale < 1) {
+			System.err.println("Scale must be at least 1");
+			return false;
+		}
+		System.out.println("Generating preview image(s) in scale 1:" + scale + "...");
+
+		if (stitch) {
+			float iScale = 1f / scale;
+
+			int w = (int) Math.ceil((512 * iScale) * stitchWidth);
+			int h = (int) Math.ceil((512 * iScale) * stitchHeight);
+
+			System.out.println("Will be stitching together individual files. Final image will be " + w + "x" + h);
+
+			stitchedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		}
 
 		boolean success = false;
 		for (File file : input) {
@@ -53,6 +90,16 @@ public class Preview implements Callable<Boolean> {
 				if (processDirectory(file)) { success = true; }
 			} else if (file.isFile()) {
 				if (processFile(file)) { success = true; }
+			}
+		}
+
+		if (stitch) {
+			try (FileOutputStream out = new FileOutputStream(new File(output, "stitched-" + System.currentTimeMillis() + ".png"))) {
+				ImageIO.write(stitchedImage, "png", out);
+				return true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
 			}
 		}
 
@@ -79,33 +126,59 @@ public class Preview implements Callable<Boolean> {
 			System.err.println("File " + file + " does not appear to be a region file (no .mca extension) - skipping");
 			return false;
 		}
-		File outFile = new File(output, file.getPath() + ".png");
-		if (outFile.exists()) {
-			return false;
-		}
-
 		System.out.println("Working on " + file + "...");
 
-		BufferedImage image;
-		try {
-			image = makeImageForRegionFile(file, scale);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+		if (stitch) {
+			// Fingers crossed people always use regular region file formats
+			String[] fileSplit = file.getName().split("\\.");
+			int regionX = Integer.parseInt(fileSplit[1]);
+			int regionZ = Integer.parseInt(fileSplit[2]);
 
-		try (FileOutputStream out = new FileOutputStream(outFile)) {
-			ImageIO.write(image, "png", out);
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
+			if (regionX < stitchStartX || regionX - stitchStartX >= stitchWidth) {
+				return false;
+			}
+			if (regionZ < stitchStartZ || regionZ - stitchStartZ >= stitchHeight) {
+				return false;
+			}
+
+			try {
+				makeImageForRegionFile(file, stitchedImage, scale, regionX - stitchStartX, regionZ - stitchStartZ);
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+		} else {
+			File outFile = new File(output, file.getPath() + ".png");
+			if (outFile.exists()) {
+				return false;
+			}
+
+			BufferedImage image;
+			try {
+				image = makeImageForRegionFile(file, scale);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+
+			try (FileOutputStream out = new FileOutputStream(outFile)) {
+				ImageIO.write(image, "png", out);
+				return true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
 		}
 	}
 
 	public static BufferedImage makeImageForRegionFile(File file, int scale) throws Exception {
 		float iScale = 1f / scale;
-		BufferedImage image = new BufferedImage((int) Math.floor(512 * iScale), (int) Math.floor(512 * iScale), BufferedImage.TYPE_INT_RGB);
+		return makeImageForRegionFile(file, new BufferedImage((int) Math.floor(512 * iScale), (int) Math.floor(512 * iScale), BufferedImage.TYPE_INT_RGB), scale, 0, 0);
+	}
+
+	public static BufferedImage makeImageForRegionFile(File file, BufferedImage image, int scale, int xOffset, int zOffset) throws Exception {
+		float iScale = 1f / scale;
 
 		try (RegionFile regionFile = new RegionFile(file)) {
 			for (int x = 0; x < 32; x++) {
@@ -136,8 +209,8 @@ public class Preview implements Callable<Boolean> {
 													color = chunk.getColorAt(xx, highest, zz);
 												}
 
-												float fX = (xx + x * 16) * iScale;
-												float fZ = (zz + z * 16) * iScale;
+												float fX = (xx + x * 16 + xOffset * 512) * iScale;
+												float fZ = (zz + z * 16 + zOffset * 512) * iScale;
 												image.setRGB((int) Math.floor(fX), (int) Math.floor(fZ), color);
 											}
 										}
